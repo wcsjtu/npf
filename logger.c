b9245b1b30f155d6c2f086d01b149beb54451377
wcsjtu <wcsjtu@163.com>
2019-05-02 01:16:21 +0800

    add log level

--- logger.c
+++ logger.c
@@ -2,6 +2,8 @@
 #include "logger.h"
 #include "globals.h"
 
+int loglv = LOGLV_DEBUG;
+
 char* strnow() {
 	time_t now;
 	struct tm buf;
@@ -13,4 +15,8 @@ char* strnow() {
 #endif
 	strftime(nowbuf, TIME_BUF_SIZE, "%Y-%m-%d %H:%M:%S", &buf);
 	return nowbuf;
+}
+
+void set_loglevel(enum LOG_LEVEL lv){
+	loglv = lv;
 }
--- logger.h
+++ logger.h
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int loglv;
 char* strnow();
 
 
@@ -14,17 +14,33 @@ char* strnow();
 #define __func__ __FUNCTION__
 #endif // WIN32
 
+enum LOG_LEVEL{
+    LOGLV_DEBUG = 0,
+    LOGLV_INFO = 10,
+    LOGLV_WARN = 20,
+    LOGLV_ERROR = 30
+};
+
+
 
 #define print(a, ...) printf("%s(%s:%d) " a,  __func__,__FILE__, __LINE__, ##__VA_ARGS__)
 #define println(a, ...) print(a "\n", ##__VA_ARGS__)
 
 
-#define logger(level, format, ...) ( printf("[%s %s:%d:%s] %s " format "\n", strnow(), __FILE__, __LINE__, __func__, level, ##__VA_ARGS__) )
+//#define logger(level, format, ...) ( fprintf(stdout, "[%s %s:%d:%s] %s " format "\n", strnow(), __FILE__, __LINE__, __func__, level, ##__VA_ARGS__) )
 
-#define logdebug(format, ...) logger("DEBUG", format, ##__VA_ARGS__ )
-#define loginfo(format, ...) logger("INFO ", format, ##__VA_ARGS__)
-#define logwarn(format, ...) logger("WARN ", format, ##__VA_ARGS__)
-#define logerror(format, ...) logger("ERROR", format, ##__VA_ARGS__)
+#define logger(level, lv, format, ...) do { \
+    if (lv >= loglv) \
+        fprintf(stdout, "[%s %s:%d:%s] %s " format "\n", strnow(), __FILE__, __LINE__, __func__, level, ##__VA_ARGS__); \
+    } while(0)
+
+
+#define logdebug(format, ...) logger("DEBUG", LOGLV_DEBUG, format, ##__VA_ARGS__ )
+#define loginfo(format, ...) logger("INFO ", LOGLV_INFO, format, ##__VA_ARGS__)
+#define logwarn(format, ...) logger("WARN ", LOGLV_WARN, format, ##__VA_ARGS__)
+#define logerror(format, ...) logger("ERROR", LOGLV_ERROR, format, ##__VA_ARGS__)
+
+void set_loglevel(enum LOG_LEVEL lv);
 
 #endif // !_LOGGER_H
 
